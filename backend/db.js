a405fcdcd6afeff6cbe7efccbedb30269eb30e5d
johngeorge1998 <noonejohn2outlook.com>
2023-01-21 11:24:07 -0800

    employee manager

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017', err =>{
+    if(!err){
+        console.log(('DB connection successful'));
+    } else{
+        console.log('Error in connection' +err);
+    }
+})
+
+module.exports =mongoose;
